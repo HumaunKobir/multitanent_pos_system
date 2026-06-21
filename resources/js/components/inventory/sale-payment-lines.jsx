499e8b02511f29f6c17e00a1af611f559a74d54e
Md Emon <mail.emon.bd123@gmail.com>
2026-06-21 12:41:19 +0600

    update

--- resources/js/components/inventory/sale-payment-lines.jsx
+++ resources/js/components/inventory/sale-payment-lines.jsx
@@ -7,7 +7,7 @@ import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 
 export function SalePaymentLines({
-    payments,
+    payments = [],
     paymentAccounts = [],
     netAmount,
     onChange,
